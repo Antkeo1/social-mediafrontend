61bac255e7c41b5b165295a76471b2990ab8c734
Antkeo1 <sleav143@gmail.com>
2019-01-28 09:54:27 -0500

    if profile is editable, will have edit and delete button, if not editable only view button

--- src/auth/components/ProfileForm.js
+++ src/auth/components/ProfileForm.js
@@ -66,65 +66,18 @@ class ProfileForm extends Component {
     let profileView
 
     if (this.props.editable){
-      profileView = <form className='auth-form' onSubmit={this.props.profileCrud === 'create' ? this.onProfileCreate : this.onProfileUpdate}>
-        <h3>Create Profile</h3>
-
-        <label htmlFor="name">Name</label>
-        <input
-          required
-          name="name"
-          value={name}
-          type="text"
-          placeholder="name"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="occupation">Occupation</label>
-        <input
-          required
-          name="occupation"
-          value={occupation}
-          type="text"
-          placeholder="Occupation"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="gender">Gender</label>
-        <input
-          required
-          name="gender"
-          value={gender}
-          type="text"
-          placeholder="Gender Identity"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="race">Race</label>
-        <input
-          required
-          name="race"
-          value={race}
-          type="text"
-          placeholder="Race"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="interest">Interest</label>
-        <input
-          required
-          name="interest"
-          value={interest}
-          type="text"
-          placeholder="What are your interest"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="hobbies">Hobbies</label>
-        <input
-          required
-          name="hobbies"
-          value={hobbies}
-          type="text"
-          placeholder="Hobbies"
-          onChange={this.handleChange}
-        />
-        <button type="submit">Create Profile</button>
-      </form>
+      profileView =
+      <div>
+        <h3>{this.props.profile.name}</h3>
+        <p>{this.props.profile.occupation}</p>
+        <p>{this.props.profile.gender}</p>
+        <p>{this.props.profile.race}</p>
+        <p>{this.props.profile.interest}</p>
+        <p>{this.props.profile.hobbies}</p>
+        <button>View</button>
+        <button>Delete</button>
+        <button>Edit</button>
+      </div>
     } else {
       profileView =
         <div>
@@ -134,8 +87,69 @@ class ProfileForm extends Component {
           <p>{this.props.profile.race}</p>
           <p>{this.props.profile.interest}</p>
           <p>{this.props.profile.hobbies}</p>
+          <button>View</button>
         </div>
     }
+
+    <form className='auth-form' onSubmit={this.props.profileCrud === 'create' ? this.onProfileCreate : this.onProfileUpdate}>
+      <h3>Create Profile</h3>
+
+      <label htmlFor="name">Name</label>
+      <input
+        required
+        name="name"
+        value={name}
+        type="text"
+        placeholder="name"
+        onChange={this.handleChange}
+      />
+      <label htmlFor="occupation">Occupation</label>
+      <input
+        required
+        name="occupation"
+        value={occupation}
+        type="text"
+        placeholder="Occupation"
+        onChange={this.handleChange}
+      />
+      <label htmlFor="gender">Gender</label>
+      <input
+        required
+        name="gender"
+        value={gender}
+        type="text"
+        placeholder="Gender Identity"
+        onChange={this.handleChange}
+      />
+      <label htmlFor="race">Race</label>
+      <input
+        required
+        name="race"
+        value={race}
+        type="text"
+        placeholder="Race"
+        onChange={this.handleChange}
+      />
+      <label htmlFor="interest">Interest</label>
+      <input
+        required
+        name="interest"
+        value={interest}
+        type="text"
+        placeholder="What are your interest"
+        onChange={this.handleChange}
+      />
+      <label htmlFor="hobbies">Hobbies</label>
+      <input
+        required
+        name="hobbies"
+        value={hobbies}
+        type="text"
+        placeholder="Hobbies"
+        onChange={this.handleChange}
+      />
+      <button type="submit">Create Profile</button>
+    </form>
 
     {/* returns my form */}
 
